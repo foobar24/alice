994f7cb631dd468a237e1004945d24717d6dd38a
Angelo Delefortrie <angelo.delefortrie@gmail.com>
2016-02-13 03:19:37 +0100

    Handle redirections

--- server.js
+++ server.js
@@ -1,19 +1,49 @@
 var httpProxy = require('http-proxy');
 var http      = require('http');
+var url       = require('url');
 
 // Website to redirect
-TARGET = 'b4wedding.fr';
+SOURCE        = 'localhost.dev:5000';
+TARGET        = 'http://www.decathlon.fr';
+PARSED_TARGET = url.parse(TARGET);
+PORT          = 5000;
 
-// Create proxy server
-var proxy = httpProxy.createProxyServer({});
+// Initialize reverse proxy
+var proxy = httpProxy.createProxyServer({ secure: false });
 
-var server = http.createServer(function(req, res) {
+// Handle proxy response
+proxy.on('proxyRes', function (proxyRes, req, res) {
 
-  req.headers['host'] = TARGET;
+  if(proxyRes.statusCode >= 301 && proxyRes.statusCode <= 302){
 
-  console.log(res.getHeader('Host'));
+    var location = proxyRes.headers['location'];
+    var replaced = location.replace(PARSED_TARGET.host, SOURCE);
 
-  proxy.web(req, res, { target: 'https://' + TARGET });
+    proxyRes.headers['location'] = replaced;
+  }
 });
 
-server.listen(5000);
+// Handle http requests
+var server = http.createServer(function(req, res) {
+
+  req.headers['host']   = PARSED_TARGET.host;
+  req.headers['origin'] = TARGET;
+
+  proxy.web(req, res, { target: TARGET });
+});
+
+// Handle errors
+proxy.on('error', function (err, req, res) {
+
+  // @todo: logs
+
+  res.writeHead(500, {
+    'Content-Type': 'text/plain'
+  });
+
+  res.end('We are sorry, but we cannot serve this request.');
+});
+
+console.log('Listen on port ' + PORT);
+
+server.listen(PORT);
