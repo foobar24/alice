5e2c83463dfe461d70048c71842701793e3f0c84
Angelo Delefortrie <angelo.delefortrie@gmail.com>
2018-03-15 11:47:30 +0100

    Code style def with eslint

--- app/tool/transform.js
+++ app/tool/transform.js
@@ -1,99 +1,101 @@
-const pipe       = require('multipipe')
-const through    = require('through')
-const iconv      = require('iconv-lite')
-const getCharset = require('charset')
-const zlib       = require('zlib')
-const config     = require('../config')
+const pipe       = require('multipipe');
+const through    = require('through');
+const iconv      = require('iconv-lite');
+const getCharset = require('charset');
+const zlib       = require('zlib');
 
 module.exports = function(transformers) {
   return function(req, res, next) {
     (function() {
-      let gunzip = zlib.createGunzip()
+      let gunzip = zlib.createGunzip();
 
-      let _writeHead = res.writeHead
+      let _writeHead = res.writeHead;
       res.writeHead = function(code, headers) {
-        let contentType = this.getHeader('content-type')
+        let contentType = this.getHeader('content-type');
 
         // Setup processor pipeline
-        let processors = []
+        let processors = [];
 
         if (typeof contentType != 'undefined') {
           for (let k in transformers) {
-            let v = transformers[k]
-            if (contentType.indexOf(k) === 0) processors.push(v())
+            let v = transformers[k];
+            if (contentType.indexOf(k) === 0) {
+              processors.push(v());
+            }
           }
         }
 
         if (!processors.length) { // nothing to do
-          _writeHead.apply(res, arguments)
-          return
+          return _writeHead.apply(res, arguments);
         }
 
         // force charset to utf8
-        let charset = getCharset(contentType)
+        let charset = getCharset(contentType);
 
         // Do nothing unless the content-type is text. (Fix #21)
-        if (!contentType.match("^text/*")) {
-          _writeHead.apply(res, arguments)
-          return
+        if (!contentType.match('^text/*')) {
+          return _writeHead.apply(res, arguments);
         }
 
-        if (res.getHeader('Content-Type') && charset)
-          res.setHeader('Content-Type', res.getHeader('Content-Type').replace(charset, ('utf8')))
+        if (res.getHeader('Content-Type') && charset) {
+          res.setHeader('Content-Type', res.getHeader('Content-Type').replace(charset, ('utf8')));
+        }
 
         // Strip off the content length since it will change.
-        res.removeHeader('Content-Length')
+        res.removeHeader('Content-Length');
 
-        if (headers) delete headers['content-length']
+        if (headers) {
+          delete headers['content-length'];
+        }
 
         // Force content type to utf8
         processors.unshift(through(function write(data) {
           if (!charset)
-            charset = getCharset(res.headers, data)
+            charset = getCharset(res.headers, data);
 
           if (charset && charset !== 'utf8') {
-            data = iconv.encode(iconv.decode(data, charset), 'utf8')
-            data = new Buffer(data.toString().replace(charset, 'utf8'))
+            data = iconv.encode(iconv.decode(data, charset), 'utf8');
+            data = new Buffer(data.toString().replace(charset, 'utf8'));
           }
 
-          this.emit('data', data)
-        }))
+          this.emit('data', data);
+        }));
 
         // Gunzip response if Gziped
-        let contentEncoding = this.getHeader('content-encoding')
+        let contentEncoding = this.getHeader('content-encoding');
         if (contentEncoding && contentEncoding.toLowerCase() == 'gzip') {
-          processors.unshift(gunzip)
+          processors.unshift(gunzip);
 
           // Strip off the content encoding since it will change.
-          res.removeHeader('Content-Encoding')
+          res.removeHeader('Content-Encoding');
 
-          if (headers) delete headers['content-encoding']
+          if (headers) delete headers['content-encoding'];
         }
 
-        let pr     = pipe([].concat.apply([], processors))
-        let _write = res.write
-        let _end   = res.end
+        let pr     = pipe([].concat.apply([], processors));
+        let _write = res.write;
+        let _end   = res.end;
 
         pr.on('data', function(buf) {
-          _write.call(res, buf)
-        })
+          _write.call(res, buf);
+        });
 
         res.write = function(data, encoding) {
-          pr.write(data, encoding)
-        }
+          pr.write(data, encoding);
+        };
 
         pr.on('end', function() {
-          _end.call(res)
-        })
+          _end.call(res);
+        });
 
         res.end = function(data, encoding) {
-          pr.end(data, encoding)
-        }
+          pr.end(data, encoding);
+        };
 
-        _writeHead.apply(res, arguments)
-      }
+        _writeHead.apply(res, arguments);
+      };
 
-      next()
-    }())
-  }
-}
+      next();
+    }());
+  };
+};
--- index.js
+++ index.js
@@ -1,61 +1,62 @@
-const httpProxy = require('http-proxy')
-const connect   = require('connect')
-const http      = require('http')
-const fs        = require('fs')
-const morgan    = require('morgan')
-const config    = require('./app/config')
+const httpProxy = require('http-proxy');
+const connect   = require('connect');
+const http      = require('http');
+const fs        = require('fs');
+const morgan    = require('morgan');
+const config    = require('./app/config');
 
 // Basic Connect App
-const app = connect()
+const app = connect();
 
 // Initialize reverse proxy
-const proxy = httpProxy.createProxyServer({ secure: false })
+const proxy = httpProxy.createProxyServer({ secure: false });
 
 // Handle proxy response
-proxy.on('proxyRes', function(proxyRes, req, res) {
+proxy.on('proxyRes', function(proxyRes) {
   if (proxyRes.statusCode >= 301 && proxyRes.statusCode <= 302) {
-    let replacer = [config.parsed_source.hostname]
+    let replacer = [config.parsed_source.hostname];
 
     if (config.parsed_source.port) {
-      replacer.push(':')
-      replacer.push(config.parsed_source.port)
+      replacer.push(':');
+      replacer.push(config.parsed_source.port);
     }
 
-    let location                 = proxyRes.headers['location']
-    let replaced                 = location.replace(config.parsed_target.host, replacer.join(''))
-    proxyRes.headers['location'] = replaced
+    let location                 = proxyRes.headers['location'];
+    let replaced                 = location.replace(config.parsed_target.host, replacer.join(''));
+    proxyRes.headers['location'] = replaced;
   }
-})
+});
 
 // Use morgan as logger
-const accessLogStream = fs.createWriteStream(__dirname + '/app.log', { flags: 'a' })
-app.use(morgan('combined', { stream: accessLogStream }))
+const accessLogStream = fs.createWriteStream(__dirname + '/app.log', { flags: 'a' });
+app.use(morgan('combined', { stream: accessLogStream }));
 
 // Handle http requests
 app.use(function(req, res, next) {
-  req.headers['host']   = config.parsed_target.host
-  req.headers['origin'] = config.target
+  req.headers['host']   = config.parsed_target.host;
+  req.headers['origin'] = config.target;
 
-  next()
-})
+  next();
+});
 
 const transforms = {
   'text/html':              require('./app/parsers').html,
   'application/javascript': require('./app/parsers').javascript
-}
+};
 
-app.use(require('./app/tool/transform')(transforms))
+app.use(require('./app/tool/transform')(transforms));
 
 app.use(function(req, res) {
-  proxy.web(req, res, { target: config.target })
-})
+  proxy.web(req, res, { target: config.target });
+});
 
 // Handle proxy errors
 proxy.on('error', function(err, req, res) {
-  res.writeHead(500, { 'Content-Type': 'text/plain' })
-  res.end('We are sorry, but we cannot serve this request.')
-})
+  res.writeHead(500, { 'Content-Type': 'text/plain' });
+  res.end('We are sorry, but we cannot serve this request.');
+});
 
 http.createServer(app).listen(config.port, function() {
-  console.log(`Server listen on http://127.0.0.1:${config.port}`)
-})
+  // eslint-disable-next-line
+  console.log(`Server listen on http://127.0.0.1:${config.port}`);
+});
